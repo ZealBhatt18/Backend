90e1d82785299399203a482098cf5cb867e75b36
zeal bhatt <zealbhatt2003@gmail.com>
2026-05-05 18:00:10 +0530

    connected with (mongodb) database for testing purpose.

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app = express();
+
+// (async()=>{
+//   try{
+//   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//   app.on("error",(error)=>{
+//     console.log("error",error);
+//     throw error
+//   })
+
+//   app.listen(process.env.PORT,()=>{console.log(`app is running on ${process.env.PORT}`);
+//   })
+//   }
+//   catch(error){
+//     console.log("Error",error);
+//     throw error
+//   }
+// })()
